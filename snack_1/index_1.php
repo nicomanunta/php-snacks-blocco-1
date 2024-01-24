c8828ac9033e1133b64c2db8dd310349a90bd16c
nicomanunta <nic.manun@gmail.com>
2024-01-24 15:20:37 +0100

    problemi snack 1

--- snack_1/index_1.php
+++ snack_1/index_1.php
@@ -1,54 +1,55 @@
 <?php
     $calendario = [
         [
-            "casa" => "Trento"
-            "ospiti" => "Brescia"
-            "punteggio_casa" => "69"
-            "punteggio_ospiti" => "93"
+            "casa" => "Trento",
+            "ospiti" => "Brescia",
+            "punteggio_casa" => "69",
+            "punteggio_ospiti" => "93",
         ],
         [
-            "casa" => "Pistoia"
-            "ospiti" => "Scafati"
-            "punteggio_casa" => "78"
-            "punteggio_ospiti" => "71"
+            "casa" => "Pistoia",
+            "ospiti" => "Scafati",
+            "punteggio_casa" => "78",
+            "punteggio_ospiti" => "71",
         ],
         [
-            "casa" => "Brindisi"
-            "ospiti" => "Napoli"
-            "punteggio_casa" => "77"
-            "punteggio_ospiti" => "80"
+            "casa" => "Brindisi",
+            "ospiti" => "Napoli",
+            "punteggio_casa" => "77",
+            "punteggio_ospiti" => "80",
         ],
         [
-            "casa" => "Varese"
-            "ospiti" => "Virtus Bologna"
-            "punteggio_casa" => "69"
-            "punteggio_ospiti" => "81"
+            "casa" => "Varese",
+            "ospiti" => "Virtus Bologna",
+            "punteggio_casa" => "69",
+            "punteggio_ospiti" => "81",
         ],
         [
-            "casa" => "Derthona"
-            "ospiti" => "Venezia"
-            "punteggio_casa" => "75"
-            "punteggio_ospiti" => "69"
+            "casa" => "Derthona",
+            "ospiti" => "Venezia",
+            "punteggio_casa" => "75",
+            "punteggio_ospiti" => "69",
         ],
         [
-            "casa" => "Reggiana"
-            "ospiti" => "Cremona"
-            "punteggio_casa" => "78"
-            "punteggio_ospiti" => "71"
+            "casa" => "Reggiana",
+            "ospiti" => "Cremona",
+            "punteggio_casa" => "78",
+            "punteggio_ospiti" => "71",
         ],
         [
-            "casa" => "Universo Treviso"
-            "ospiti" => "Dinamo Sassari"
-            "punteggio_casa" => "77"
-            "punteggio_ospiti" => "70"
+            "casa" => "Universo Treviso",
+            "ospiti" => "Dinamo Sassari",
+            "punteggio_casa" => "77",
+            "punteggio_ospiti" => "70",
         ],
         [
-            "casa" => "VL Pesaro"
-            "ospiti" => "Olimpia Milano"
-            "punteggio_casa" => "65"
-            "punteggio_ospiti" => "85"
+            "casa" => "VL Pesaro",
+            "ospiti" => "Olimpia Milano",
+            "punteggio_casa" => "65",
+            "punteggio_ospiti" => "85",
         ]
     ]
+    
 ?>
 
 <!DOCTYPE html>
@@ -63,8 +64,12 @@
     Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
     Olimpia Milano - Cantù | 55-60 -->
 <body>
-        <?php
-            foreach ($calendario as $partite)
-        ?>
+    <ul>
+        <?php foreach ($calendario as $partite){ ?>
+            <li>
+                <?php echo $partite["casa"] ." - ". $partite["ospiti"] ." | ". $partite["punteggio_casa"] ."-". $partite["punteggio_ospiti"];?>
+            </li>
+        <?php}?>
+    </ul>
 </body>
 </html>
