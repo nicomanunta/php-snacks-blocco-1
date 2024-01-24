392feabb96baed1e9ce1ed8183a630d43d21dc40
nicomanunta <nic.manun@gmail.com>
2024-01-24 14:51:39 +0100

    Aggiunto array calendario

--- snack_1/index_1.php
+++ snack_1/index_1.php
@@ -1,3 +1,56 @@
+<?php
+    $calendario = [
+        [
+            "casa" => "Trento"
+            "ospiti" => "Brescia"
+            "punteggio_casa" => "69"
+            "punteggio_ospiti" => "93"
+        ],
+        [
+            "casa" => "Pistoia"
+            "ospiti" => "Scafati"
+            "punteggio_casa" => "78"
+            "punteggio_ospiti" => "71"
+        ],
+        [
+            "casa" => "Brindisi"
+            "ospiti" => "Napoli"
+            "punteggio_casa" => "77"
+            "punteggio_ospiti" => "80"
+        ],
+        [
+            "casa" => "Varese"
+            "ospiti" => "Virtus Bologna"
+            "punteggio_casa" => "69"
+            "punteggio_ospiti" => "81"
+        ],
+        [
+            "casa" => "Derthona"
+            "ospiti" => "Venezia"
+            "punteggio_casa" => "75"
+            "punteggio_ospiti" => "69"
+        ],
+        [
+            "casa" => "Reggiana"
+            "ospiti" => "Cremona"
+            "punteggio_casa" => "78"
+            "punteggio_ospiti" => "71"
+        ],
+        [
+            "casa" => "Universo Treviso"
+            "ospiti" => "Dinamo Sassari"
+            "punteggio_casa" => "77"
+            "punteggio_ospiti" => "70"
+        ],
+        [
+            "casa" => "VL Pesaro"
+            "ospiti" => "Olimpia Milano"
+            "punteggio_casa" => "65"
+            "punteggio_ospiti" => "85"
+        ]
+    ]
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +59,12 @@
     <title>Document</title>
     <link rel="stylesheet" href="./css_1/style_1.css">
 </head>
+    <!-- ## Snack 1
+    Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+    Olimpia Milano - Cantù | 55-60 -->
 <body>
-    
+        <?php
+            foreach ($calendario as $partite)
+        ?>
 </body>
 </html>
